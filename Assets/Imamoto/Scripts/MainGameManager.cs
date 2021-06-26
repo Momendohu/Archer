a96741a9de5e564a064eb17bccf6743aad3649e3
@siroinaba <siroinaba@gmail.com>
2021-06-26 18:35:53 +0900

    神マネージャー追加

---
+++ Assets/Imamoto/Scripts/MainGameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        None = 0,
+        Title = 1,
+        MainGameStart = 2,
+        MainGameFisnih = 3,
+    }
+
+    public void GameStart()
+    {
+        _state = GameState.MainGameStart;
+    }
+
+    public void GameFisnih()
+    {
+        _state = GameState.MainGameFisnih;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _state = GameState.Title;
+    }
+
+    private GameState _state;
+}
