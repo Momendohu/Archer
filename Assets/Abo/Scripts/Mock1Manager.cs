655afd09309406574319d856dc58d77f411c49fc
Momendohu <ueuetenango@ueuetenangonoMacBook-Pro.local>
2021-11-06 17:20:48 +0900

    feat:とりあえず音鳴らした

---
+++ Assets/Abo/Scripts/Mock1Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mock1Manager : MonoBehaviour {
+    void Awake () {
+        AudioManager.Instance.PlayBGM ("sample1");
+    }
+
+    void Start () {
+
+    }
+
+    void Update () {
+
+    }
+}
